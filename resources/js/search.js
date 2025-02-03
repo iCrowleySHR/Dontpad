27fb8d4cf44379e46f689b3bebee8d4cb25129d6
Gustavo Gualda <104951600+iCrowleySHR@users.noreply.github.com>
2025-02-03 00:15:32 -0300

    feat

--- resources/js/search.js
+++ resources/js/search.js
@@ -1,24 +1,65 @@
 import './bootstrap';
+const input = document.getElementById('search');
+const btn = document.getElementById('btn');
 
-document.getElementById('search').addEventListener('input', function() {
+input.addEventListener('input', function() {
     const query = this.value;
     const suggestions = document.getElementById('suggestions');
 
     if (query.length >= 1) {
         axios.get("/search", { params: { query } })
             .then(response => {
-                suggestions.innerHTML = response.data.map(page => `
-                <a href="/${page}" target="_blank">
-                    <li class="py-3 px-4 hover:bg-gray-200 hover:rounded-md transition duration-150 ease-in-out">
-                        ${page}
-                    </li>
-                </a>
-                `).join('');
-                suggestions.classList.remove('hidden');
+                if (response.data.length > 0) {
+                    suggestions.innerHTML = response.data.map(page => `
+                        <a href="/${page}" target="_blank">
+                            <li class="py-3 px-4 hover:bg-gray-200 hover:rounded-md transition duration-150 ease-in-out">
+                                ${page}
+                            </li>
+                        </a>
+                    `).join('');
+                    suggestions.classList.remove('hidden');
+
+                    btn.textContent = 'Search';
+                    input.classList.remove('focus:ring-green-500');
+                    input.classList.add('focus:ring-blue-500');
+                    btn.classList.remove('bg-green-500');
+                    btn.classList.add('bg-blue-500');
+                } else {
+                    btn.textContent = 'Create';
+                    btn.classList.remove('bg-blue-500');
+                    btn.classList.add('bg-green-500');
+                    input.classList.remove('focus:ring-blue-500');
+                    input.classList.add('focus:ring-green-500');
+
+                    suggestions.innerHTML = '';
+                    suggestions.classList.add('hidden');
+                }
             })
             .catch(console.error);
     } else {
         suggestions.innerHTML = '';
-        suggestions.classList.add('hidden'); 
+        suggestions.classList.add('hidden');
+
+        btn.textContent = 'Search';
+        btn.classList.remove('bg-green-500');
+        btn.classList.add('bg-blue-500');
+        input.classList.remove('focus:ring-green-500');
+        input.classList.add('focus:ring-blue-500');
     }
 });
+
+input.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+        const query = this.value;
+        if (query.length >= 1) {
+            window.location.href = `/${query}`;
+        }
+    }
+});
+
+btn.addEventListener('click', function() {
+    const query = input.value;
+    if (query.length >= 1) {
+        window.location.href = `/${query}`;             
+    }
+})  
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,8 +5,15 @@
 @section('content')
 <main class="flex justify-center h-screen">
     <section class="container lg:w-1/4 relative mx-5 mt-[20vh]">
-        <h1 class="text-2xl font-semibold mb-4 text-center">Dontpad</h1>
-        <input type="text" id="search" class="border border-gray-300 p-3 w-full rounded-lg shadow-md focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-200" placeholder="Search for pages...">
+        <h1 class="text-5xl font-semibold text-center">DONTPAD</h1>
+        <h2 class="text-center mb-4">The simplest way to share text online.</h2>
+        
+        <div class="flex space-x-2">
+            <input type="text" id="search" class="border border-gray-300 p-3 w-full rounded-lg shadow-md focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-200" placeholder="Search for pages or create...">
+            <button id="btn" class="bg-blue-500 text-white px-4 py-1 rounded-lg shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-200">
+                Search
+            </button>
+        </div>
         
         <ul id="suggestions" class="bg-white border border-gray-300 mt-2 rounded-lg shadow-md max-h-60 overflow-y-auto absolute w-full z-10 hidden"></ul>
     </section>
